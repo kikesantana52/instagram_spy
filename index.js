e2b80c1a688186c1ac4c6b96b23a86c5519fc9f6
Luis Enrique Santana Del Pozo <kikesantana52@gmail.com>
2018-01-31 14:04:24 -0600

    Cambios
    (destructuring, var -> let and const, scope globl eliminado)

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-var str_url = 'https://www.instagram.com/daniellachavezofficial/?__a=1';
-var obj_instagram = fetch(str_url);
-var str_biog, str_username;
-var arr_posts;
+let str_url = 'https://www.instagram.com/daniellachavezofficial/?__a=1';
+const obj_instagram = fetch(str_url);
+
 var arr_copy;
 
 obj_instagram.then((data)=>{ return data.json();})
-          .then((data) => {
-            str_username = data.user.username;
-            str_biog = data.user.biography;
-            console.log(str_username);
-            console.log(str_biog);
-            arr_posts = data.user.media.nodes;
-            arr_copy = {...arr_posts};
-             var arr_sortable = [];
-             for (var post in arr_copy) {
-                 arr_sortable.push([arr_copy[post].likes.count, arr_copy[post].caption,arr_copy[post].comments.count]);
-             }
-             arr_sortable.sort((a,b)=>{
-               return b[0] - a[0];
-             });
-             for (var i = 0;  i < arr_sortable.length; i++) {
-                console.log(`# of likes: ${arr_sortable[i][0]}, Caption: ${arr_sortable[i][1]}, # of comments: ${arr_sortable[i][2]}`);
-              }
-
-          });
+.then((data) => {
+  const {username : str_username, biography :str_biog} = data.user;
+  console.log(str_username);
+  console.log(str_biog);
+  let arr_posts = [...data.user.media.nodes];
+  arr_posts.sort((a,b)=>{
+    return b.likes.count - a.likes.count;
+  });
+  arr_posts.forEach((post)=>{
+    console.log(`Likes: ${post.likes.count}, Caption: ${post.caption}, Comments: ${post.comments.count}`);
+  });
+});
